f9db0e104b270fd5435c18a239cd12c82b0b5835
1-Dmytro-Shynkarenko <114245538+1-Dmytro-Shynkarenko@users.noreply.github.com>
2023-04-23 16:39:06 +0300

    hw2

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar  } from 'expo-status-bar';
+import { StyleSheet, ImageBackground, View, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import React, { useState } from "react";
+import RegistrationScreen from './Screens/RegistrationScreen/RegistrationScreen';
+import LoginScreen from './Screens/LoginScreen/LoginScreen'
+
+const backImage = require('./Source/Photo_BG.png');
 
 export default function App() {
+
+   const [activeScreen, setActiveScreen] = useState(0);
+   const changeScrennFunc = (value) => { setActiveScreen (value) }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Запрацювало! Нарешті!</Text>
-      <StatusBar style="auto" />
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <View style={styles.maincontainer}>
+      <ImageBackground source={backImage} style={styles.backImg} >
+        { activeScreen=== 0 ? <LoginScreen changeScrenn={ changeScrennFunc }/> :
+         <RegistrationScreen changeScrenn={ changeScrennFunc }/>}
+        </ImageBackground>
+        <StatusBar style="auto" />  
     </View>
-  );
+  </TouchableWithoutFeedback>);
 }
 
 const styles = StyleSheet.create({
-  container: {
+  maincontainer: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+  }, 
+  backImg: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    width: '100%'
   },
 });
